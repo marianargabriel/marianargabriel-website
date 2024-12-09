a919ea4c608e5abe6a4ca27a46ebf7c7e8d34b56
Mariana Gabriel <marianagabriel.2004@gmail.com>
2024-12-09 12:40:46 +0000

    Alteração na navbar
    Adição de links ancora e adicao de menu hamburguer para responsividade

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,10 +1,17 @@
 'use client';
 
 import React from 'react';
+import { useState } from 'react';
 
 import '@/public/styles/navbar.css';
 
 export default function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
         <>
             <nav className="navbar">
@@ -15,11 +22,18 @@ export default function Navbar() {
                             width={70}
                             height={64} />
                     </a>
-                    <a href="https://api.whatsapp.com/send?phone=351924306673" target="_blank">
-                        <button type="button" className="btn btn-outline-primary btn-talkToMe">
-                            Talk to Me
-                        </button>
-                    </a>
+                    <div className={`nav-container ${menuOpen ? 'active' : ''}`}>
+                        <a href="#about" className="nav-link">About Me</a>
+                        <a href="#portfolio" className="nav-link">Portfolio</a>
+                        <a href="https://api.whatsapp.com/send?phone=351924306673" target="_blank" rel="noopener noreferrer">
+                            <button type="button" className="btn btn-outline-primary btn-talkToMe">
+                                Talk to Me
+                            </button>
+                        </a>
+                    </div>
+                    <div className="hamburger" onClick={toggleMenu}>
+                        <i className="bi bi-list"></i>
+                    </div>
                 </div>
             </nav>
         </>
